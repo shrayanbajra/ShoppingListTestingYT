ae248af590f1b0eae92df5b75aba2091f8b06a4e
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2020-09-22 17:53:07 +0545

    Created FakeRepository

---
+++ app/src/test/java/com/androiddevs/shoppinglisttestingyt/repository/FakeRepository.kt
@@ -0,0 +1,62 @@
+package com.androiddevs.shoppinglisttestingyt.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItem
+import com.androiddevs.shoppinglisttestingyt.data.remote.response.ImageResponse
+import com.androiddevs.shoppinglisttestingyt.utils.Resource
+
+class FakeRepository : ShoppingRepository {
+
+    private val shoppingItems = mutableListOf<ShoppingItem>()
+
+    private val observableShoppingItem = MutableLiveData<List<ShoppingItem>>(shoppingItems)
+    private val observableTotalPrice = MutableLiveData<Float>()
+
+    private var hasNetworkError = false
+
+    fun setNetworkError(value: Boolean) {
+        hasNetworkError = value
+    }
+
+    override suspend fun insertShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.add(shoppingItem)
+        refreshLiveData()
+    }
+
+    override suspend fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.remove(shoppingItem)
+        refreshLiveData()
+    }
+
+    private fun refreshLiveData() {
+        observableShoppingItem.postValue(shoppingItems)
+        val totalPrice = getTotalPrice()
+        observableTotalPrice.postValue(totalPrice)
+    }
+
+    private fun getTotalPrice(): Float {
+        return shoppingItems.sumByDouble { it.price.toDouble() }.toFloat()
+    }
+
+    override fun observeAllShoppingItems(): LiveData<List<ShoppingItem>> {
+        return observableShoppingItem
+    }
+
+    override fun observeTotalPrice(): LiveData<Float> {
+        return observableTotalPrice
+    }
+
+    override suspend fun searchForImage(imageQuery: String): Resource<ImageResponse> {
+        return if (hasNetworkError) {
+
+            Resource.error("Error", null)
+
+        } else {
+
+            val imageResponse = ImageResponse(listOf(), 10, 1)
+            Resource.success(imageResponse)
+
+        }
+    }
+}
